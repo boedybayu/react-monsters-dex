976ef7c23855e1ca613755df001e02cc64ce3d93
Bayu <boedy.bayu@gmail.com>
2021-04-20 14:39:29 +0700

    Add Search box and handle styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { Component } from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     };
   }
 
@@ -17,9 +19,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase());
+    });
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
